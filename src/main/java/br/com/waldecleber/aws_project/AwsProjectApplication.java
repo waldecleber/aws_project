87a49303d60553bfa81e3cd14aca9a3662a46b06
Waldecleber <waldecleber@gmail.com>
2023-12-01 14:48:30 -0300

    Initial commit

---
+++ src/main/java/br/com/waldecleber/aws_project/AwsProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.waldecleber.aws_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/waldecleber/aws_project/AwsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.waldecleber.aws_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
